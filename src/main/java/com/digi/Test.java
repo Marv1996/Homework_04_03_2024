a2dfa8854c3287ed979c0d9ad0ca3900df7d8159
Marv1996 <122868168+Marv1996@users.noreply.github.com>
2024-03-11 13:43:26 +0400

    Update Test.java

--- src/main/java/com/digi/Test.java
+++ src/main/java/com/digi/Test.java
@@ -4,20 +4,23 @@ import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(4, 5, 11, 12, 8, 15, 9, 13, 16));
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(4, 5, 11, 12, 8, 15, 9, 13, 16, 25, -5, 10, 10, 10));
 
         List<Integer> listIndex = new ArrayList<>();
         int target = 20;
         Map<Integer, Integer> integerHashMap = new HashMap<>();
 
+        int index = 0;
         for (int num : numbers) {
             int result = target - num;
+
             if (integerHashMap.containsKey(num)) {
-                listIndex.add(numbers.indexOf(integerHashMap.get(num)));
-                listIndex.add(numbers.indexOf(num));
+                listIndex.add(integerHashMap.get(num));
+                listIndex.add(index);
             } else {
-                integerHashMap.put(result, num);
+                integerHashMap.put(result, index);
             }
+            index++;
         }
         System.out.println(listIndex);
     }
